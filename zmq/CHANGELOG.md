9e9835a31ee4a0cfb8f5ecaa63baab61682f531d
CESTS-HF-D-025\Anil M <anilkumar.murikipudi@cestechservices.com>
2018-01-10 14:40:18 +0530

    zmq changes

---
+++ zmq/CHANGELOG.md
@@ -0,0 +1,22 @@
+CHANGELOG
+=========
+
+* 0.3.1 (2014-xx-xx)
+
+  * Change layout to PSR-4
+
+* 0.3.0 (2014-05-25)
+
+  * Bump to react 0.4
+
+* 0.2.0 (2013-09-18)
+
+  * Bump to react 0.3
+
+* 0.1.1 (2012-11-18)
+
+  * Feature: Emit `messages` instead of `message` for multi-part receive
+
+* 0.1.0 (2012-10-23)
+
+  * Bug fix: Resolve flaw in reading of edge-triggered events
---
+++ zmq/src/SocketWrapper.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace React\ZMQ;
+
+use Evenement\EventEmitter;
+use React\EventLoop\LoopInterface;
+use ZMQ;
+use ZMQSocket;
+
+/**
+ * @mixin ZMQSocket
+ */
+class SocketWrapper extends EventEmitter
+{
+    /**
+     * @var resource
+     */
+    public $fileDescriptor;
+
+    /**
+     * @var bool
+     */
+    public $closed = false;
+
+    /**
+     * @var ZMQSocket
+     */
+    protected $socket;
+
+    /**
+     * @var LoopInterface
+     */
+    protected $loop;
+
+    /**
+     * @var Buffer
+     */
+    protected $buffer;
+
+    /**
+     * @param ZMQSocket     $socket
+     * @param LoopInterface $loop
+     */
+    public function __construct(ZMQSocket $socket, LoopInterface $loop)
+    {
+        $this->socket = $socket;
+        $this->loop = $loop;
+
+        $this->fileDescriptor = $this->socket->getSockOpt(ZMQ::SOCKOPT_FD);
+
+        $writeListener = array($this, 'handleEvent');
+
+        $this->buffer = new Buffer($socket, $this->fileDescriptor, $this->loop, $writeListener);
+    }
+
+    public function attachReadListener()
+    {
+        $this->loop->addReadStream($this->fileDescriptor, array($this, 'handleEvent'));
+    }
+
+    public function handleEvent()
+    {
+        while (true) {
+            $events = $this->socket->getSockOpt(ZMQ::SOCKOPT_EVENTS);
+
+            $isPollIn = $events & ZMQ::POLL_IN;
+            $isPollOut = $events & ZMQ::POLL_OUT;
+
+            $hasEvents = $isPollIn || ($isPollOut && $this->buffer->listening);
+
+            if (!$hasEvents) {
+                break;
+            }
+
+            if ($isPollIn) {
+                $this->handleReadEvent();
+            }
+
+            if ($isPollOut && $this->buffer->listening) {
+                $this->buffer->handleWriteEvent();
+            }
+        }
+    }
+
+    public function handleReadEvent()
+    {
+        $messages = $this->socket->recvmulti(ZMQ::MODE_NOBLOCK);
+
+        if ($messages !== false) {
+            if (count($messages) === 1) {
+                $this->emit('message', array($messages[0]));
+            }
+
+            $this->emit('messages', array($messages));
+        }
+    }
+
+    /**
+     * @return ZMQSocket
+     */
+    public function getWrappedSocket()
+    {
+        return $this->socket;
+    }
+
+    /**
+     * @param string $channel
+     */
+    public function subscribe($channel)
+    {
+        $this->socket->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, $channel);
+    }
+
+    /**
+     * @param string $channel
+     */
+    public function unsubscribe($channel)
+    {
+        $this->socket->setSockOpt(ZMQ::SOCKOPT_UNSUBSCRIBE, $channel);
+    }
+
+    /**
+     * @param string $message
+     */
+    public function send($message)
+    {
+        $this->buffer->send($message);
+    }
+
+    public function close()
+    {
+        if ($this->closed) {
+            return;
+        }
+
+        $this->emit('end', array($this));
+        $this->loop->removeStream($this->fileDescriptor);
+        $this->buffer->removeAllListeners();
+        $this->removeAllListeners();
+        unset($this->socket);
+        $this->closed = true;
+    }
+
+    public function end()
+    {
+        if ($this->closed) {
+            return;
+        }
+
+        $that = $this;
+
+        $this->buffer->on('end', function () use ($that) {
+            $that->close();
+        });
+
+        $this->buffer->end();
+    }
+
+    /**
+     * @param string $method
+     * @param array  $parameters
+     *
+     * @return mixed
+     */
+    public function __call($method, array $parameters)
+    {
+        return call_user_func_array(array($this->socket, $method), $parameters);
+    }
+}
